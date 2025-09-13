656d0e438c4ad8d54f14447ec76e7868f5ad1cce
MiguelPachon <miguel.pachon@estudiantesunibague.edu.co>
2025-09-13 09:19:48 -0500

    filtros

---
+++ app/map/page.js
@@ -0,0 +1,10 @@
+"use client";
+import MapView from "../dashboard/components/MapView"; // Ajusta la ruta según tu proyecto
+
+export default function MapPage() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <MapView />
+    </div>
+  );
+}
