852419e9d5b06e28f385fcd911c0e5fad78ed58d
MiguelPachon <miguel.pachon@estudiantesunibague.edu.co>
2025-09-13 01:23:58 -0500

    prueba

---
+++ app/login/page.js
@@ -0,0 +1,59 @@
+'use client'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { supabase } from '../../lib/supabaseClient'
+
+export default function LoginPage() {
+  const router = useRouter()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState(null)
+
+  
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    setError(null)
+
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    })
+
+    if (error) {
+      setError(error.message)
+    } else {
+      router.push('/dashboard')
+    }
+  }
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <form onSubmit={handleLogin} className="bg-white p-6 rounded shadow w-96">
+        <h2 className="text-2xl font-bold mb-4">Iniciar Sesión</h2>
+        {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+        <input
+          type="email"
+          placeholder="Correo"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full border p-2 mb-3 rounded"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full border p-2 mb-3 rounded"
+          required
+        />
+        <button
+          type="submit"
+          className="w-full bg-amber-400 text-white py-2 rounded"
+        >
+          Ingresar
+        </button>
+      </form>
+    </div>
+  )
+}
